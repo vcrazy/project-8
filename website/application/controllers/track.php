9a90cdb844329a7a944e762ae953755d4d25fd03
Vihren Ganev <vcrazy@abv.bg>
2014-04-26 16:58:21 +0300

    track

--- website/application/controllers/track.php
+++ website/application/controllers/track.php
@@ -1,7 +1,25 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+class Track extends MY_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model('Model_track');
+	}
+
+	public function index()
+	{
+		if(isset($_GET['phone_id']) && isset($_GET['campaign_id']))
+		{
+			$result = $this->Model_track->insert($_GET['phone_id'], (int)$_GET['campaign_id']);
+
+			echo json_encode(array('ok' => (int)$result));
+		}
+		else
+		{
+			echo json_encode(array('ok' => 0));
+		}
+	}
+}
