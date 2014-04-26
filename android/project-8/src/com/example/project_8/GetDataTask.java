0ef555b17658bebba1beb0d80619ffc7c7466362
rhristova <rhristova@despark.com>
2014-04-26 12:20:49 +0300

    Clean again, Ado

--- android/project-8/src/com/example/project_8/GetDataTask.java
+++ android/project-8/src/com/example/project_8/GetDataTask.java
@@ -1,12 +1,22 @@
 package com.example.project_8;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.AsyncTask;
 
 public class GetDataTask extends AsyncTask<Void, Void, Void> {
 
+	private static final String URL = "";
+
 	@Override
 	protected Void doInBackground(Void... params) {
-		// TODO Auto-generated method stub
+
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpPost httpPost;
+		HttpResponse response;
+
 		return null;
 	}
 
