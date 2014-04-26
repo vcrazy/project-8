c948182044ef43dd982bb43fa81eba382511b743
clanrbr <clan_rbr@yahoo.com>
2014-04-26 17:18:29 +0300

    changed

--- android/project-8/src/com/example/project_8/MainActivity.java
+++ android/project-8/src/com/example/project_8/MainActivity.java
@@ -2,40 +2,22 @@ package com.example.project_8;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.widget.ListView;
 
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.SherlockActivity;
-
-public class MainActivity extends SherlockActivity implements
-		ActionBar.TabListener {
+public class MainActivity extends Activity {
 	private ListView mlistView;
 	private CustomAdapter adapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		setTheme(com.actionbarsherlock.R.style.Sherlock___Theme); // Used for
-																	// theme
-																	// switching
-																	// in
-																	// samples
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.tab_navigation);
 		mlistView = (ListView) findViewById(R.id.list_view);
 		loadData();
 		mlistView.setAdapter(adapter);
-
-		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		for (int i = 1; i <= 4; i++) {
-			ActionBar.Tab tab = getSupportActionBar().newTab();
-			tab.setText("Tab " + i);
-			tab.setTabListener(this);
-			getSupportActionBar().addTab(tab);
-		}
 	}
 
 	public void loadData() {
@@ -49,19 +31,6 @@ public class MainActivity extends SherlockActivity implements
 		list.add(obj1);
 		list.add(obj2);
 		this.adapter = new CustomAdapter(this, R.layout.list_item, list);
-	}
-
-	@Override
-	public void onTabReselected(Tab tab, FragmentTransaction transaction) {
-	}
-
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction transaction) {
-		// mSelected.setText("Selected: " + tab.getText());
-	}
-
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction transaction) {
 	}
 
 }
