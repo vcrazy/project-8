18f233ab649442387532476314f598cd5fb2417e
rhristova <rhristova@despark.com>
2014-04-26 12:11:15 +0300

    clean 2

---
+++ android/project-8/src/com/example/project_8/GetDataTask.java
@@ -0,0 +1,13 @@
+package com.example.project_8;
+
+import android.os.AsyncTask;
+
+public class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+	@Override
+	protected Void doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- android/project-8/src/com/example/project_8/MainActivity.java
+++ android/project-8/src/com/example/project_8/MainActivity.java
@@ -1,66 +1,49 @@
 package com.example.project_8;
 
-<<<<<<< HEAD
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockActivity;
-
-public class MainActivity extends SherlockActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-	}
-=======
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockActivity;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.TextView;
-
-
-public class MainActivity extends SherlockActivity implements ActionBar.TabListener {
+public class MainActivity extends SherlockActivity implements
+		ActionBar.TabListener {
 	private TextView mSelected;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        setTheme(com.actionbarsherlock.R.style.Sherlock___Theme); //Used for theme switching in samples
-        super.onCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		setTheme(com.actionbarsherlock.R.style.Sherlock___Theme); // Used for
+																	// theme
+																	// switching
+																	// in
+																	// samples
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.tab_navigation);
-        mSelected = (TextView)findViewById(R.id.text);
+		setContentView(R.layout.tab_navigation);
+		mSelected = (TextView) findViewById(R.id.text);
 
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        for (int i = 1; i <= 3; i++) {
-            ActionBar.Tab tab = getSupportActionBar().newTab();
-            tab.setText("Tab " + i);
-            tab.setTabListener(this);
-            getSupportActionBar().addTab(tab);
-        }
-    }
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		for (int i = 1; i <= 3; i++) {
+			ActionBar.Tab tab = getSupportActionBar().newTab();
+			tab.setText("Tab " + i);
+			tab.setTabListener(this);
+			getSupportActionBar().addTab(tab);
+		}
+	}
 
-    @Override
-    public void onTabReselected(Tab tab, FragmentTransaction transaction) {
-    }
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction transaction) {
+	}
 
-    @Override
-    public void onTabSelected(Tab tab, FragmentTransaction transaction) {
-        mSelected.setText("Selected: " + tab.getText());
-    }
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction transaction) {
+		mSelected.setText("Selected: " + tab.getText());
+	}
 
-    @Override
-    public void onTabUnselected(Tab tab, FragmentTransaction transaction) {
-    }
->>>>>>> 2b0275ccb5efc60c168ce74e5f3c2c05e680dd8a
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction transaction) {
+	}
 
 }
