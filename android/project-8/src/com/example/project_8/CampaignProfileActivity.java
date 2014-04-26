3c1a0bd45a59b38486e3ef6b5e396da85d626e8b
clanrbr <clan_rbr@yahoo.com>
2014-04-26 18:54:33 +0300

    added clicable things and sms send

--- android/project-8/src/com/example/project_8/CampaignProfileActivity.java
+++ android/project-8/src/com/example/project_8/CampaignProfileActivity.java
@@ -1,7 +1,11 @@
 package com.example.project_8;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,8 +14,11 @@ public class CampaignProfileActivity extends Activity {
 	/* Widgets */
 	private ImageView imageView;
 	private TextView textViewName;
-	private TextView textViewDate;
+	private TextView textViewcampaignSubName;
 	private TextView textViewDescription;
+	private TextView textViewStartDate;
+	private TextView textViewEndDate;
+	private Button btnSendSms;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,26 @@ public class CampaignProfileActivity extends Activity {
 		/* Get widgets */
 		imageView = (ImageView) findViewById(R.id.imageView);
 		textViewName = (TextView) findViewById(R.id.name);
-		textViewDate = (TextView) findViewById(R.id.date);
+		textViewcampaignSubName = (TextView) findViewById(R.id.campaignSubName);
 		textViewDescription = (TextView) findViewById(R.id.description);
+		textViewStartDate = (TextView) findViewById(R.id.startDate);
+		textViewEndDate = (TextView) findViewById(R.id.endDate);
+		btnSendSms = (Button) findViewById(R.id.send_sms);
 
+		btnSendSms.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				sendSMS("17777", "msg");
+			}
+		});
 	}
 
+	private void sendSMS(String formattedNumbers, String txtSms) {
+		// with intent
+		String uri = "smsto:" + formattedNumbers;
+		Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(uri));
+		smsIntent.putExtra("sms_body", txtSms);// F1RST
+		// SMS
+		smsIntent.putExtra("compose_mode", true);
+		startActivity(smsIntent);
+	}
 }
--- android/project-8/src/com/example/project_8/MainActivity.java
+++ android/project-8/src/com/example/project_8/MainActivity.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private ListView mlistView;
 	private CustomAdapter adapter;
+	private TextView textViewPeople;
+	private TextView textViewOrganisation;
+	private TextView textViewSpecial;
+	private TextView textViewOther;
 
 	private ArrayList<FullInfo> fullList = new ArrayList<FullInfo>();
 
@@ -19,6 +26,57 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.tab_navigation);
 		mlistView = (ListView) findViewById(R.id.list_view);
+		textViewPeople = (TextView) findViewById(R.id.tab_people);
+		textViewPeople.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textViewOther.setSelected(false);
+				textViewSpecial.setSelected(false);
+				textViewOrganisation.setSelected(false);
+				textViewPeople.setSelected(true);
+			};
+		});
+
+		textViewOrganisation = (TextView) findViewById(R.id.tab_organisations);
+		textViewOrganisation.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textViewOther.setSelected(false);
+				textViewSpecial.setSelected(false);
+				textViewOrganisation.setSelected(true);
+				textViewPeople.setSelected(false);
+			};
+		});
+
+		textViewSpecial = (TextView) findViewById(R.id.tab_special);
+		textViewSpecial.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textViewOther.setSelected(false);
+				textViewSpecial.setSelected(true);
+				textViewOrganisation.setSelected(false);
+				textViewPeople.setSelected(false);
+			};
+		});
+
+		textViewOther = (TextView) findViewById(R.id.tab_other);
+		textViewOther.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				textViewOther.setSelected(true);
+				textViewSpecial.setSelected(false);
+				textViewOrganisation.setSelected(false);
+				textViewPeople.setSelected(false);
+			};
+		});
 		loadData();
 		mlistView.setAdapter(adapter);
 	}
